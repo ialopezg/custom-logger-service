fad77b4cfa5e3093292340e78aebf2e28ef18e20
Isidro A. Lopez G <me@ialopezg.com>
2020-04-29 05:51:51 -0500

    Add debug, error, info and warn logger

--- src/logger.js
+++ src/logger.js
@@ -1,38 +1,121 @@
+const dateFormat = require('dateformat')
 const chalk = require('chalk')
 const ip = require('ip')
 
 const divider = chalk.gray('\n-----------------------------------')
-/**
- * Logger middleware, you can customize it to make messages more personal.
- *
- * @type {{appStarted: logger.appStarted, error: logger.error}}
- */
-const logger = {
-  /**
-   * Called whenever there's an error on the server we want to print.
-   *
-   * @param err
-   */
-  error: err => {
-    console.error(chalk.red(err))
-  },
 
-  appStarted: (port, host, tunnel) => {
-    console.log(`Server started ! ${chalk.green('✓')}`)
+var events = {}
 
-    // If the tunnel started, log the URL it's available at
-    if (tunnel) {
-      console.log(`Tunnel initialised ${chalk.green('✓')}`)
+var options = {
+  level: 0,
+  format: '%timestamp% - %event%:%padding%  %message%',
+  timestamp: 'HH:MM:ss',
+  messageFormatting: false
+}
+
+function logger (options) {
+  this.event = options.event
+  this.level = options.level || 0
+  this.color = options.color || 'white'
+}
+
+logger.prototype.config = function (config) {
+  for (var key in config) {
+    this[key] = config[key]
+  }
+  return this
+}
+
+logger.prototype.__defineGetter__('padding', function () {
+  var length = 0
+  var padding = ''
+
+  for (var key in events) {
+    if (events.hasOwnProperty(key)) {
+      length =
+        length < events[key].event.length ? events[key].event.length : length
     }
+  }
+  for (var i = 0; i < length - this.event.length; i++) {
+    padding += ' '
+  }
 
-    console.log(`
+  return padding
+})
+
+logger.prototype.output = function (input) {
+  if (options.level > this.level) {
+    return
+  }
+  var message = ''
+  if (options.messageFormatting) {
+    message = ' ' + util.format.apply(util, input)
+  } else {
+    for (var key in input) {
+      message +=
+        ' ' +
+        (typeof input[key] === 'object'
+          ? JSON.stringify(input[key], null)
+          : input[key])
+    }
+  }
+  var format = this.format || options.format
+  var output = format
+    .replace(
+      '%timestamp%',
+      dateFormat(new Date(), this.timestamp || options.timestamp)
+    ) // timestamp
+    .replace('%event%', chalk.keyword(this.color).bold(this.event)) // log event & color
+    .replace('%padding%', this.padding)
+    .replace('%message%', chalk.keyword(this.color)(message))
+
+  console.log(output)
+}
+
+exports.config = function (config) {
+  for (var key in config) {
+    if (options.hasOwnProperty(key)) {
+      options[key] = config[key]
+    }
+  }
+  return this
+}
+
+exports.appStarted = function (port, host, tunnel) {
+  console.log(`Server started ! ${chalk.green('✓')}`)
+
+  // If the tunnel started, log the URL it's available at
+  if (tunnel) {
+    console.log(`Tunnel initialised ${chalk.green('✓')}`)
+  }
+
+  console.log(`
 ${chalk.bold('Access URLs:')}${divider}
 Localhost: ${chalk.magenta(`http://${host}:${port}`)}
       LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
         (tunnel ? `\n    Proxy: ${chalk.magenta(tunnel)}` : '')}${divider}
 ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
     `)
+}
+
+function nFn (e) {
+  return function () {
+    return arguments.length === 0 ? events[e] : events[e].output(arguments)
   }
 }
 
-module.exports = logger
+exports.new = function (newEvents) {
+  for (var event in newEvents) {
+    events[event] = new logger(newEvents[event])
+    this[event] = nFn(event)
+  }
+
+  return this
+}
+
+exports.new({
+  debug: { level: 0, event: 'debug', color: 'grey' },
+  error: { level: 1, event: 'error', color: 'red' },
+  info: { level: 2, event: 'info', color: 'green' },
+  warn: { level: 3, event: 'warn', color: 'yellow' }
+})
--- src/tests/logger.spec.js
+++ src/tests/logger.spec.js
@@ -1,9 +1,36 @@
 const logger = require('../logger')
 
-test('Must print a debugger message by a console', () => {
+it('Must setup configurations for a logger instance', () => {
+  const spy = jest.spyOn(logger, 'config').mockImplementation(() => {})
+
+  const config = Object.assign({
+    level: 0,
+    event: 'info',
+    color: 'green'
+  })
+  logger.config(config)
+
+  expect(logger.config).toBeCalled()
+  expect(config).toMatchSnapshot()
+})
+
+it('Must print a debugger message by a console in grey color', () => {
+  const spy = jest.spyOn(logger, 'debug').mockImplementation(() => {})
+
+  const message = 'Console Debugging testing message'
+  logger.debug(message)
+
+  expect(logger.debug).toBeCalled()
+  expect(spy.mock.calls[0][0]).toContain(message)
+  expect(spy.mock.calls[0][0]).toMatchSnapshot()
+
+  spy.mockRestore()
+})
+
+test('Must print a error message by a console in red color', () => {
   const spy = jest.spyOn(logger, 'error').mockImplementation(() => {})
 
-  const message = 'Console Error testing record'
+  const message = 'Console Error testing message'
   logger.error(message)
 
   expect(logger.error).toBeCalled()
@@ -13,10 +40,36 @@ test('Must print a debugger message by a console', () => {
   spy.mockRestore()
 })
 
-test('Must be an informative message', () => {
+it('Must print a info message by a console in green color', () => {
+  const spy = jest.spyOn(logger, 'info').mockImplementation(() => {})
+
+  const message = 'Console Informative testing message'
+  logger.info(message)
+
+  expect(logger.info).toBeCalled()
+  expect(spy.mock.calls[0][0]).toContain(message)
+  expect(spy.mock.calls[0][0]).toMatchSnapshot()
+
+  spy.mockRestore()
+})
+
+it('Must print a warning message by a console in yellow color', () => {
+  const spy = jest.spyOn(logger, 'warn').mockImplementation(() => {})
+
+  const message = 'Console Error testing message'
+  logger.warn(message)
+
+  expect(logger.warn).toBeCalled()
+  expect(spy.mock.calls[0][0]).toContain(message)
+  expect(spy.mock.calls[0][0]).toMatchSnapshot()
+
+  spy.mockRestore()
+})
+
+it('Must be an informative message', () => {
   const spy = jest.spyOn(logger, 'appStarted').mockImplementation(() => {})
 
-  logger.appStarted(3000, 'localhost')
+  logger.appStarted(3000, 'localhost', true)
 
   expect(logger.appStarted).toBeCalled()
   expect(true).toMatchSnapshot()
