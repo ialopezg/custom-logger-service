779de321c7b363ac23f5a7a54d02aae30138670e
Isidro A. López G <me@ialopezg.com>
2022-05-30 09:42:41 -0500

    fix: update dependencies in example

--- src/test/common/utils/padding.util.spec.ts
+++ src/test/common/utils/padding.util.spec.ts
@@ -7,15 +7,32 @@ describe('Utilities', () => {
 
     it('should add space to right to a text when keys to be compare are provided', () => {
       const keys = Object.keys(LogLevel);
-      const output = padding(Object.values(LogLevel)[LogLevel.DEBUG].toString(), keys);
+      const output = padding(
+        Object.values(LogLevel)[LogLevel.DEBUG].toString(),
+        keys,
+      );
       const lenght = output.length;
 
-      expect(padding(Object.values(LogLevel)[LogLevel.DEBUG].toString(), keys)).toEqual(output);
-      expect(padding(Object.values(LogLevel)[LogLevel.DEBUG].toString(), keys).length).toEqual(lenght);
-      expect(padding(Object.values(LogLevel)[LogLevel.ERROR].toString(), keys).length).toEqual(lenght);
-      expect(padding(Object.values(LogLevel)[LogLevel.INFO].toString(), keys).length).toEqual(lenght);
-      expect(padding(Object.values(LogLevel)[LogLevel.LOG].toString(), keys).length).toEqual(lenght);
-      expect(padding(Object.values(LogLevel)[LogLevel.WARN].toString(), keys).length).toEqual(lenght);
+      expect(
+        padding(Object.values(LogLevel)[LogLevel.DEBUG].toString(), keys),
+      ).toEqual(output);
+      expect(
+        padding(Object.values(LogLevel)[LogLevel.DEBUG].toString(), keys)
+          .length,
+      ).toEqual(lenght);
+      expect(
+        padding(Object.values(LogLevel)[LogLevel.ERROR].toString(), keys)
+          .length,
+      ).toEqual(lenght);
+      expect(
+        padding(Object.values(LogLevel)[LogLevel.INFO].toString(), keys).length,
+      ).toEqual(lenght);
+      expect(
+        padding(Object.values(LogLevel)[LogLevel.LOG].toString(), keys).length,
+      ).toEqual(lenght);
+      expect(
+        padding(Object.values(LogLevel)[LogLevel.WARN].toString(), keys).length,
+      ).toEqual(lenght);
     });
 
     it('should a text without padding when no keys to compare are provided', () => {
